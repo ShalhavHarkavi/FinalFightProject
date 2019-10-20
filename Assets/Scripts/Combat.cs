a5c23e0effd73a38334febab8f7fe745fced5c15
Shalhav Harkavi <shalhav2@gmail.com>
2019-10-20 21:24:55 +0300

    Second punch doesn't instantly fire w/out input anymore

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -4,38 +4,30 @@ using UnityEngine;
 
 public class Combat : MonoBehaviour
 {
-    int punchCounter;
     Animator animator;
-    [SerializeField] List<string> animationTriggerNames;
     [SerializeField] bool isHitstunned = false;
-
     [SerializeField] float attackInitDelay = .5f;
-    [SerializeField] public float maxattackInitDelay = 0.5f;
     bool canInitiate = true;
 
     void Start()
     {
-        punchCounter = 0;
         animator = GetComponent<Animator>();
-
-        attackInitDelay = maxattackInitDelay;
     }
     public void InitiatePunchSystem(Collider2D enemyCollider) //Fix so punches don't go off automatically.
     {
         if (canInitiate)
         {
             canInitiate = false;
-            if (punchCounter >= animationTriggerNames.Count || !isHitStunned(enemyCollider.gameObject))
+            if (!isHitStunned(enemyCollider.gameObject))
             {
-                punchCounter = 0;
+                enemyCollider.gameObject.GetComponent<Combat>().isHitstunned = true;
                 return; //before return - add a bit of stun. unless it is already part of the uppercut animation
             }
             else if (isHitStunned(enemyCollider.gameObject))
             {
                 animator.SetTrigger("isPunching");
                 Invoke("ResetAttack", attackInitDelay);
-                punchCounter++;
-                return;   
+                return;
             }
             
         }
