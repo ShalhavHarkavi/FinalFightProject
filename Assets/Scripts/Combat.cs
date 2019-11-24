c0ffcd58f4b639188782e328745fb6eae6811ac6
Shalhav Harkavi <shalhav2@gmail.com>
2019-11-24 12:21:11 +0200

    updated state machine (no jump)

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -6,7 +6,7 @@ public class Combat : MonoBehaviour
 {
     Animator animator;
     [SerializeField] bool isHitstunned = false;
-    [SerializeField] float attackInitDelay = .5f;
+    [SerializeField] float attackInitDelay = 5f;
     bool canInitiate = true;
     [SerializeField] float hitstunTimerMax = 300f;
     float hitstunTimer;
@@ -20,20 +20,23 @@ public class Combat : MonoBehaviour
     }
     void Update()
     {
-        if (isHitstunned)
-        {
-            hitstunTimer -= 1;
-        }
-        if (hitstunTimer <= 0)
-        {
-            isHitstunned = false;
-            hitstunTimer = hitstunTimerMax;
-        }
         // if (isHitstunned)
         // {
-        //     //add set stun animation
-        //     Invoke("ResetHitstun", hitstunTimer);
+        //     hitstunTimer -= 1;
         // }
+        // if (hitstunTimer <= 0)
+        // {
+        //     isHitstunned = false;
+        //     hitstunTimer = hitstunTimerMax;
+        // }
+        if (isHitstunned)
+        {
+            //add set stun animation
+            Invoke("ResetHitstun", hitstunTimer);
+        }
+
+        if (this.gameObject.CompareTag("Enemy"))
+            Debug.Log(hitstunTimer);
     }
     public void InitiatePunchSystem(Collider2D enemyCollider) //Fix so punches don't go off automatically.
     {
@@ -45,9 +48,11 @@ public class Combat : MonoBehaviour
                 enemyCollider.gameObject.GetComponent<Combat>().isHitstunned = true;
                 return; //before return - add a bit of stun. unless it is already part of the uppercut animation
             }
-            else if (isHitStunned(enemyCollider.gameObject) && Input.GetButtonDown("Punch"))
+            else if (isHitStunned(enemyCollider.gameObject) && (Input.GetButtonDown("Punch") || Input.GetButtonDown("Heavy Punch") || Input.GetButtonDown("Uppercut")))
             {
-                animator.SetTrigger("isPunching");
+                if (Input.GetButtonDown("Punch")) animator.SetTrigger("isPunching");
+                else if (Input.GetButtonDown("Heavy Punch")) animator.SetTrigger("isHardPunching");
+                else if (Input.GetButtonDown("Uppercut")) animator.SetTrigger("isUppercutting");
                 Invoke("ResetAttack", attackInitDelay);
                 return;
             }
