c930487fdd9e28cc9299f270f1f647ccb11cd096
Shalhav Harkavi <shalhav2@gmail.com>
2020-01-04 22:38:24 +0200

    COMBAT WORKS (need to find why getting nullrefexception when exiting trigger with consumable item in Combat.cs)

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -6,7 +6,7 @@ public class Combat : MonoBehaviour
 {
   Animator animator;
   [SerializeField] bool isHitstunned = false;
-  [SerializeField] float attackInitDelay = 5f;
+  [SerializeField] float attackInitDelay = 0.0001f;
   bool canInitiate = true;
   [SerializeField] float hitstunTimerTest = 5f; //In seconds!!!!
   [SerializeField] float hitstunTimerLight = 300f;
@@ -14,6 +14,7 @@ public class Combat : MonoBehaviour
   [SerializeField] float hitstunTimerHeavy = 300f;
   [SerializeField] float hitstunTimerAir = 300f;
   private float currentHitstunTimer;
+  bool canInvokeHitstunReset, wasHit;
 
   //add property descriptions for inspector
 
@@ -21,6 +22,8 @@ public class Combat : MonoBehaviour
   {
     animator = GetComponent<Animator>();
     currentHitstunTimer = 0f;
+    canInvokeHitstunReset = true;
+    wasHit = false;
   }
   void Update()
   {
@@ -33,13 +36,22 @@ public class Combat : MonoBehaviour
     //   isHitstunned = false;
     //   hitstunTimer = hitstunTimerMax;
     // }
-    if (isHitstunned)
+    if (isHitstunned && canInvokeHitstunReset)
     {
       //add set stun animation
+      canInvokeHitstunReset = false;
       Invoke("ResetHitstun", currentHitstunTimer);
     }
+    if (isHitstunned && !canInvokeHitstunReset && wasHit)
+    {
+      //Stun anim again? Maybe put in function?
+      CancelInvoke("ResetHitstun");
+      Invoke("ResetHitstun", currentHitstunTimer);
+    }
+    // if (!isHitstunned)
+    //   wasHit = false;
     // if (this.gameObject.CompareTag("Enemy"))
-    //   Debug.Log(hitstunTimer);
+    //   Debug.Log(isHitstunned);
   }
   public void InitiatePunchSystem(Collider2D enemyCollider) //Fix so punches don't go off automatically.
   {
@@ -51,31 +63,44 @@ public class Combat : MonoBehaviour
       //   Debug.Log("Punch");
       // Need to find solutions for different hitstuns for all the different moves
       Combat enemyCombatComp = enemyCollider.transform.parent.gameObject.GetComponent<Combat>(); // Handle errors here if no combat component ound or if no parent and such
+      enemyCombatComp.SetWasHit(true);
       if (!enemyCombatComp.GetIsHitsunned())
       {
         enemyCombatComp.SetCurrentHitstun(this.hitstunTimerTest); // Temporary hitstun for testing
         enemyCombatComp.SetIsHitstunned(true);
-        return; //before return - add a bit of stun. unless it is already part of the uppercut animation
+        // return; //before return - add a bit of stun. unless it is already part of the uppercut animation
       }
       else if (enemyCombatComp.GetIsHitsunned() && (Input.GetButtonDown("Punch") || Input.GetButtonDown("Heavy Punch") || Input.GetButtonDown("Uppercut")))
       {
         if (Input.GetButtonDown("Punch")) animator.SetTrigger("isPunching");
         else if (Input.GetButtonDown("Heavy Punch")) animator.SetTrigger("isHardPunching");
         else if (Input.GetButtonDown("Uppercut")) animator.SetTrigger("isUppercutting");
-        Invoke("ResetAttack", attackInitDelay);
-        return;
+        // Invoke("ResetAttack", attackInitDelay); //Maybe just caninit = true? without invoke?
+        // return;
       }
-      
+      // Invoke("ResetAttack", attackInitDelay); //Maybe just caninit = true? without invoke?
+      canInitiate = true;
     }
   }
+  private void OnTriggerExit2D(Collider2D enemyCollider) {
+    if (!enemyCollider.transform.parent.gameObject)
+      return;
+    if (!enemyCollider.transform.parent.gameObject.CompareTag("Enemy"))
+      return;
+    Combat enemyCombatComp = enemyCollider.transform.parent.gameObject.GetComponent<Combat>();
+    enemyCombatComp.SetWasHit(false);
+  }
+  
   private void ResetAttack() { canInitiate = true; }
   // private bool isHitStunned(GameObject enemy) { return enemy.GetComponent<Combat>().isHitstunned; }
   private void ResetHitstun()
   {
     isHitstunned = false;
+    canInvokeHitstunReset = true;
     //turn off stun animation
   }
   public void SetCurrentHitstun(float newCurrentHitstun) { this.currentHitstunTimer = newCurrentHitstun; }
   public void SetIsHitstunned(bool newHitstunStatus) { this.isHitstunned = newHitstunStatus; }
   public bool GetIsHitsunned() { return this.isHitstunned; }
+  public void SetWasHit(bool newWasHitStatus) { this.wasHit = newWasHitStatus; } 
 }
